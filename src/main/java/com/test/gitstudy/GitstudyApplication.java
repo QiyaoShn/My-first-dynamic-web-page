275febcb527d73cbf1bb4dbb373b8c9299c2d0ae
QiyaoShn <18272214732@163.com>
2021-09-04 16:28:32 +0800

    init

---
+++ src/main/java/com/test/gitstudy/GitstudyApplication.java
@@ -0,0 +1,13 @@
+package com.test.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitstudyApplication.class, args);
+    }
+
+}
